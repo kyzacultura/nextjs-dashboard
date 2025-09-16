bbc3acf247556d85d467f4e285c5bc7c8d8b044b
Kryza Anne Cultura <kyzacultura28@gmail.com>
2025-09-16 17:42:54 +0800

    increased font size

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <h1 className="text-blue-500">I'm Kryza Anne!</h1>
+          <h1 className="text-4xl text-blue-500">I'm Kryza Anne!</h1>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
