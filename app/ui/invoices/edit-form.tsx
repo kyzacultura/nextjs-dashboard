187955accc7fec7b4fab7fdd1db3ad7e1d829fbd
Kryza Anne Cultura <kyzacultura28@gmail.com>
2025-11-30 23:58:14 +0800

    updated delete, edit, create

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -17,10 +17,14 @@ export default function EditInvoiceForm({
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
-}) { const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
- 
-  return (<form action={updateInvoiceWithId}>
+}) {
+  // Bind invoice ID to the server action
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
+  return (
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
+
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -52,18 +56,16 @@ export default function EditInvoiceForm({
             Choose an amount
           </label>
           <div className="relative mt-2 rounded-md">
-            <div className="relative">
-              <input
-                id="amount"
-                name="amount"
-                type="number"
-                step="0.01"
-                defaultValue={invoice.amount}
-                placeholder="Enter USD amount"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
+            <input
+              id="amount"
+              name="amount"
+              type="number"
+              step="0.01"
+              defaultValue={invoice.amount}
+              placeholder="Enter USD amount"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+            />
+            <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
         </div>
 
@@ -90,6 +92,7 @@ export default function EditInvoiceForm({
                   Pending <ClockIcon className="h-4 w-4" />
                 </label>
               </div>
+
               <div className="flex items-center">
                 <input
                   id="paid"
@@ -110,6 +113,8 @@ export default function EditInvoiceForm({
           </div>
         </fieldset>
       </div>
+
+      {/* Buttons */}
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
